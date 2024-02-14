f0f2b82359ffdf406d57295a9fb56aebf9dc71a3
TheAlexDev23 <thealexdev23@gmail.com>
2024-02-14 10:46:31 +0000

    refactor: migrate db type errors to thiserror

--- src/db/errors.rs
+++ src/db/errors.rs
@@ -1,30 +1,13 @@
+use thiserror::Error;
+
 use diesel::result::Error as QueryError;
-use std::fmt;
-use std::fmt::Display;
 
 /// Error for performing any package db query that involves
 /// json serialization/deserialization at any point
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum TranslatedPackageQueryError {
-    Query(QueryError),
-    Json(serde_json::Error),
-}
-impl From<QueryError> for TranslatedPackageQueryError {
-    fn from(other: QueryError) -> Self {
-        TranslatedPackageQueryError::Query(other)
-    }
-}
-impl From<serde_json::Error> for TranslatedPackageQueryError {
-    fn from(other: serde_json::Error) -> Self {
-        TranslatedPackageQueryError::Json(other)
-    }
-}
-
-impl Display for TranslatedPackageQueryError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            TranslatedPackageQueryError::Query(error) => write!(f, "{error}"),
-            TranslatedPackageQueryError::Json(error) => write!(f, "{error}"),
-        }
-    }
+    #[error("A query error has occured: {0}")]
+    Query(#[from] QueryError),
+    #[error("A json serialization error has occured: {0}")]
+    Json(#[from] serde_json::Error),
 }
